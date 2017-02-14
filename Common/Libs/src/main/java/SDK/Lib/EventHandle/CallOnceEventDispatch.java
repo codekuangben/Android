1962b1f07f70f9dac5b2b4299ca67d420b5df175
codekuangben <kuangben2001@163.com>
2017-02-14 22:07:37 +0800

    aaa

---
+++ Common/Libs/src/main/java/SDK/Lib/EventHandle/CallOnceEventDispatch.java
@@ -0,0 +1,19 @@
+﻿namespace SDK.Lib
+{
+    /**
+     * @brief 一次事件分发，分发一次就清理
+     */
+    public class CallOnceEventDispatch : EventDispatch
+    {
+        public CallOnceEventDispatch()
+        {
+
+        }
+
+        override public void dispatchEvent(IDispatchObject dispatchObject)
+        {
+            base.dispatchEvent(dispatchObject);
+            clearEventHandle();
+        }
+    }
+}

009a331402f6b1714ff344491d50ea8595b73938
codekuangben <kuangben2001@163.com>
2017-02-19 01:56:21 +0800

    aaa

--- Test/TestLibs/Libs/src/main/java/SDK/Lib/DataStruct/MsgBuffer.java
+++ Test/TestLibs/Libs/src/main/java/SDK/Lib/DataStruct/MsgBuffer.java
@@ -1,5 +1,8 @@
 ﻿package SDK.Lib.DataStruct;
 
+import SDK.Lib.FrameWork.MacroDef;
+import SDK.Lib.Tools.EEndian;
+
 public class MsgBuffer
 {
     protected CircularBuffer mCircularBuffer;  // 环形缓冲区
@@ -7,35 +10,36 @@ public class MsgBuffer
     protected ByteBuffer mHeaderBA;            // 主要是用来分析头的大小
     protected ByteBuffer mMsgBodyBA;           // 返回的字节数组
 
-    public MsgBuffer(uint initCapacity = BufferCV.INIT_CAPACITY, uint maxCapacity = BufferCV.MAX_CAPACITY)
+    public MsgBuffer()
+    {
+        this(BufferCV.INIT_CAPACITY, BufferCV.MAX_CAPACITY);
+    }
+
+    public MsgBuffer(int initCapacity)
+    {
+        this(initCapacity, BufferCV.MAX_CAPACITY);
+    }
+
+    public MsgBuffer(int initCapacity, int maxCapacity)
     {
         mCircularBuffer = new CircularBuffer(initCapacity, maxCapacity);
         mHeaderBA = new ByteBuffer();
         mMsgBodyBA = new ByteBuffer();
     }
 
-    public ByteBuffer headerBA
+    public ByteBuffer getHeaderBA()
     {
-        get
-        {
-            return mHeaderBA;
-        }
+        return mHeaderBA;
     }
 
-    public ByteBuffer msgBodyBA
+    public ByteBuffer getMsgBodyBA()
     {
-        get
-        {
-            return mMsgBodyBA;
-        }
+        return mMsgBodyBA;
     }
 
-    public CircularBuffer circularBuffer
+    public CircularBuffer getCircularBuffer()
     {
-        get
-        {
-            return mCircularBuffer;
-        }
+        return mCircularBuffer;
     }
 
     // 设置网络字节序
@@ -48,11 +52,11 @@ public class MsgBuffer
     /**
      * @brief 检查 CB 中是否有一个完整的消息
      */
-    protected bool checkHasMsg()
+    protected boolean checkHasMsg()
     {
         mCircularBuffer.frontBA(mHeaderBA, MsgCV.HEADER_SIZE);  // 将数据读取到 mHeaderBA
-        uint msglen = 0;
-        mHeaderBA.readUnsignedInt32(ref msglen);
+        int msglen = 0;
+        mHeaderBA.readUnsignedInt32(msglen);
         if (MacroDef.MSG_COMPRESS)
         {
             if ((msglen & MsgCV.PACKET_ZIP) > 0)         // 如果有压缩标志
@@ -60,7 +64,7 @@ public class MsgBuffer
                 msglen &= (~MsgCV.PACKET_ZIP);         // 去掉压缩标志位
             }
         }
-        if (msglen <= mCircularBuffer.size - MsgCV.HEADER_SIZE)
+        if (msglen <= mCircularBuffer.getSize() - MsgCV.HEADER_SIZE)
         {
             return true;
         }
@@ -73,14 +77,14 @@ public class MsgBuffer
     /**
      * @brief 获取前面的第一个完整的消息数据块
      */
-    public bool popFront()
+    public boolean popFront()
     {
-        bool ret = false;
-        if (mCircularBuffer.size > MsgCV.HEADER_SIZE)         // 至少要是 DataCV.HEADER_SIZE 大小加 1 ，如果正好是 DataCV.HEADER_SIZE ，那只能说是只有大小字段，没有内容
+        boolean ret = false;
+        if (mCircularBuffer.getSize() > MsgCV.HEADER_SIZE)         // 至少要是 DataCV.HEADER_SIZE 大小加 1 ，如果正好是 DataCV.HEADER_SIZE ，那只能说是只有大小字段，没有内容
         {
             mCircularBuffer.frontBA(mHeaderBA, MsgCV.HEADER_SIZE);  // 如果不够整个消息的长度，还是不能去掉消息头的
-            uint msglen = 0;
-            mHeaderBA.readUnsignedInt32(ref msglen);
+            int msglen = 0;
+            mHeaderBA.readUnsignedInt32(msglen);
             if (MacroDef.MSG_COMPRESS)
             {
                 if ((msglen & MsgCV.PACKET_ZIP) > 0)         // 如果有压缩标志
@@ -89,7 +93,7 @@ public class MsgBuffer
                 }
             }
 
-            if (msglen <= mCircularBuffer.size - MsgCV.HEADER_SIZE)
+            if (msglen <= mCircularBuffer.getSize() - MsgCV.HEADER_SIZE)
             {
                 mCircularBuffer.popFrontLen(MsgCV.HEADER_SIZE);
                 mCircularBuffer.popFrontBA(mMsgBodyBA, msglen);
@@ -108,15 +112,15 @@ public class MsgBuffer
     /**
      * @brief KBEngine 引擎消息处理
      */
-    public bool popFrontAll()
+    public boolean popFrontAll()
     {
-        bool ret = false;
+        boolean ret = false;
 
         if (!mCircularBuffer.empty())
         {
             ret = true;
             mCircularBuffer.linearize();
-            mCircularBuffer.popFrontBA(mMsgBodyBA, mCircularBuffer.size);
+            mCircularBuffer.popFrontBA(mMsgBodyBA, mCircularBuffer.getSize());
             mCircularBuffer.clear();
         }
 
--- Test/TestLibs/Libs/src/main/java/SDK/Lib/DataStruct/MsgCV.java
+++ Test/TestLibs/Libs/src/main/java/SDK/Lib/DataStruct/MsgCV.java
@@ -2,7 +2,7 @@
 
 public class MsgCV
 {
-    public final int PACKET_ZIP_MIN = 32;
-    public final int PACKET_ZIP = 0x40000000;
-    public final int HEADER_SIZE = 4;   // 包长度占据几个字节
+    public static final int PACKET_ZIP_MIN = 32;
+    public static final int PACKET_ZIP = 0x40000000;
+    public static final int HEADER_SIZE = 4;   // 包长度占据几个字节
 }

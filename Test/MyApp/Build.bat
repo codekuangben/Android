a2cf6ea4a81926fc27bd54e7de18765cf1601796
codekuangben <kuangben2001@163.com>
2017-06-04 23:21:33 +0800

    aaa

--- Test/MyApp/Build.bat
+++ Test/MyApp/Build.bat
@@ -14,10 +14,10 @@ cd %BASE_DIR%
 :: set path envionment variable
 set path=%path%;"F:\ProgramFiles\android\Android Studio\gradle\gradle-2.14.1\bin"
 
-call gradlew.bat -v
-call gradlew.bat tasks
+::call gradlew.bat -v
+::call gradlew.bat tasks
 call gradlew.bat clean
 call gradlew.bat build
-call gradlew.bat compileReleaseSource
+::call gradlew.bat compileReleaseSource
 
 pause

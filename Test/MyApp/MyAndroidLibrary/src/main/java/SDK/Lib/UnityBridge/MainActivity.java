255bb4cea5a293207c55a4e5db6c87e7d61e4bc0
codekuangben <kuangben2001@163.com>
2017-06-04 23:46:19 +0800

    aaa

---
+++ Test/MyApp/MyAndroidLibrary/src/main/java/SDK/Lib/UnityBridge/MainActivity.java
@@ -0,0 +1,30 @@
+package SDK.Lib.UnityBridge;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import com.unity3d.player.UnityPlayer;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class MainActivity extends UnityPlayerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    // 显示Toast消息
+    public void ShowToast(final String message){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    // 返回一个字符串（静态方法）
+    public static String GetInformation()
+    {
+        return "This is a Plugin's content!";
+    }
+}
